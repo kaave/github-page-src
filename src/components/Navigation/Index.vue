60e22c1e856e67eff60c0f53f714e8a93e42165d
kaave <abe@framelunch.jp>
2019-05-05 23:51:41 +0900

    nav: add

--- src/components/Navigation/Index.vue
+++ src/components/Navigation/Index.vue
@@ -1,14 +1,35 @@
 <!-- eslint-disable -->
 <template>
-  <nav class="Navigation">
+  <!-- eslint-disable -->
+  <nav class="Navigation" :class="!isInitialize ? '-not-init' : ''">
     <div class="Navigation__content -hidden-pc">
       <hamburger @click="handleHambergerClick" :is-close="isShowSpMenu" />
     </div>
-    <transition name="sp-menu" :duration="600" mode="out-in">
-      <sp-modal-menu v-if="isShowSpMenu" :menus="menus" :snses="snses" />
+    <transition name="sp-menu" :duration="600">
+      <aside v-if="isShowSpMenu" class="Navigation__sp-modal-menu">
+        <ul class="Navigation__sp-menu">
+          <li v-for="menu in menus" :key="menu" class="Navigation__sp-menu-cell">
+            <nuxt-link :to="`/${menu}`" class="Navigation__sp-menu-link">{{ menu }}</nuxt-link>
+          </li>
+        </ul>
+        <ul class="Navigation__sp-sns">
+          <li v-for="{ key, url } in sns" :key="key" class="Navigation__sp-sns-cell">
+            <a :href="url" class="Navigation__sp-sns-link" target="_blank" rel="noopener">
+              <component :is="`icon-${key}`" class="Navigation__sp-sns-icon" />
+            </a>
+          </li>
+        </ul>
+      </aside>
     </transition>
-    <pc-menu class="-hidden-sp" :menus="menus" :snses="snses" />
+    <div class="Navigation__content -hidden-sp">
+      <ul class="Navigation__pc-menu">
+        <li v-for="menu in menus" :key="menu" class="Navigation__pc-menu-cell">
+          <nuxt-link :to="`/${menu}`" class="Navigation__pc-menu-link">{{ menu }}</nuxt-link>
+        </li>
+      </ul>
+    </div>
   </nav>
+  <!-- eslint-enable -->
 </template>
 <!-- eslint-enable -->
 
@@ -16,18 +37,19 @@
 import Vue from 'vue';
 import throttle from 'lodash/throttle';
 
-import Hamburger from './Hamburger.vue';
-import SpModalMenu from './SpModalMenu.vue';
-import PcMenu from './PcMenu.vue';
+import IconFacebook from '../Icon/Facebook.vue';
+import IconTwitter from '../Icon/Twitter.vue';
+import IconGithub from '../Icon/GitHub.vue';
+import IconWantedly from '../Icon/Wantedly.vue';
 
-type Data = { isShowSpMenu: boolean };
+type Data = { isShowSpMenu: boolean; isInitialize: boolean };
 type Methods = { handleHambergerClick: () => void };
 type Computed = { menus: string[]; snses: { key: string; url: string; desc: string }[] };
 type Props = {};
 
-const defaultData: Data = { isShowSpMenu: false };
+const defaultData: Data = { isShowSpMenu: false, isInitialize: false };
 
-const components = { Hamburger, SpModalMenu, PcMenu };
+const components = { IconFacebook, IconTwitter, IconGithub, IconWantedly };
 
 export default Vue.extend<Data, Methods, Computed, Props>({
   components,
@@ -47,7 +69,9 @@ export default Vue.extend<Data, Methods, Computed, Props>({
       ];
     },
   },
-  mounted() {},
+  mounted() {
+    setTimeout(() => (this.isInitialize = true), 50);
+  },
   methods: {
     handleHambergerClick: throttle(function handleHambergerClick() {
       // @ts-ignore
@@ -59,13 +83,20 @@ export default Vue.extend<Data, Methods, Computed, Props>({
 
 <style lang="scss" scoped>
 .Navigation {
-  z-index: nth($zLevels, 8);
-  position: fixed;
-  display: block;
-  top: 0;
-  left: 0;
-  width: 100%;
-  padding: 4vw;
+  @include sp {
+    position: fixed;
+    display: block;
+    top: 0;
+    left: 0;
+    width: 100%;
+    padding: 4vw;
+  }
+}
+
+.Navigation__content.-hidden-pc {
+  display: flex;
+  justify-content: flex-end;
+}
 
   @include notSp {
     padding: 0;
@@ -91,6 +122,148 @@ export default Vue.extend<Data, Methods, Computed, Props>({
 
   @include maxSize {
     font-size: 3.2rem;
+  }
+}
+
+.Navigation__sp-modal-menu {
+  color: $colorWhite;
+  font-size: 3.75vw;
+  margin-top: 1em;
+}
+
+.Navigation__sp-menu {
+  margin: 0;
+  text-align: right;
+}
+
+.Navigation__sp-menu-cell {
+  margin: 0.5em 0 0;
+}
+
+.Navigation__sp-menu-link {
+  display: inline-block;
+
+  &,
+  &:hover,
+  &:active {
+    color: currentColor;
+    text-decoration: none;
+  }
+}
+
+.sp-menu-enter .Navigation__sp-menu-cell {
+  transform: translate3d(10vw, 0, 0);
+  opacity: 0;
+}
+
+.sp-menu-enter-active .Navigation__sp-menu-cell {
+  transition: all 200ms $easeOutQuad;
+}
+
+.sp-menu-leave-active .Navigation__sp-menu-cell {
+  transform: translate3d(10vw, 0, 0);
+  opacity: 0;
+  transition: all 400ms $easeOutQuad;
+}
+
+.sp-menu-enter-active,
+.sp-menu-leave-active {
+  .Navigation__sp-menu-cell:first-child {
+    transition-delay: 100ms;
+  }
+
+  .Navigation__sp-menu-cell:nth-child(2) {
+    transition-delay: 150ms;
+  }
+
+  .Navigation__sp-menu-cell:last-child {
+    transition-delay: 200ms;
+  }
+}
+
+.Navigation__sp-sns {
+  display: flex;
+  justify-content: space-between;
+  width: 7.5em;
+  margin: 2em 0 0 auto;
+}
+
+.sp-menu-enter .Navigation__sp-sns {
+  transform: translate3d(10vw, 0, 0);
+  opacity: 0;
+}
+
+.sp-menu-enter-active .Navigation__sp-sns {
+  transition: all 200ms 250ms $easeOutQuad;
+}
+
+.sp-menu-leave-active .Navigation__sp-sns {
+  transform: translate3d(10vw, 0, 0);
+  opacity: 0;
+  transition: all 400ms 250ms $easeOutQuad;
+}
+
+.Navigation__sp-sns-cell {
+  margin: 0;
+}
+
+.Navigation__sp-sns-link {
+  display: block;
+
+  &,
+  &:hover,
+  &:active {
+    color: currentColor;
+    text-decoration: none;
+  }
+}
+
+.Navigation__sp-sns-icon {
+  width: 1.5em;
+  height: 1.5em;
+  fill: currentColor;
+}
+
+.Navigation__pc-menu {
+  position: absolute;
+  top: 50%;
+  left: 1em;
+  transform: translateY(-50%);
+  margin: 0;
+  font-size: 1.6rem;
+}
+
+.Navigation__pc-menu-cell {
+  margin: 1em 0 0;
+  transition: all 400ms $easeOutQuad;
+}
+
+.Navigation__pc-menu-cell:first-child {
+  margin-top: 0;
+  transition-delay: 100ms;
+}
+
+.Navigation__pc-menu-cell:nth-child(2) {
+  transition-delay: 150ms;
+}
+
+.Navigation__pc-menu-cell:last-child {
+  transition-delay: 200ms;
+}
+
+.-not-init .Navigation__pc-menu-cell {
+  transform: translate3d(-10rem, 0, 0);
+  opacity: 0;
+}
+
+.Navigation__pc-menu-link {
+  display: inline-block;
+
+  &,
+  &:hover,
+  &:active {
+    color: currentColor;
+    text-decoration: none;
   }
 }
 </style>
