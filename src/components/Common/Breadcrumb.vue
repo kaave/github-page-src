c98d618d9864e05a95736be866b1ae770f6b27a3
kaave <abe@framelunch.jp>
2019-05-16 17:59:58 +0900

    common: add breadcrumb(mock)

---
+++ src/components/Common/Breadcrumb.vue
@@ -0,0 +1,140 @@
+<!-- eslint-disable -->
+<template>
+  <nav class="Breadcrumb">
+    <div class="Breadcrumb__inner">
+      <ol class="List">
+        <li class="Cell">
+          <nuxt-link :to="`/`" class="Link">top</nuxt-link>
+        </li>
+        <li class="Cell">
+          <nuxt-link :to="`/entries`" class="Link">entries</nuxt-link>
+        </li>
+        <li class="Cell">
+          <span class="Current">entries</span>
+        </li>
+      </ol>
+    </div>
+  </nav>
+</template>
+<!-- eslint-enable -->
+
+<style lang="scss" scoped>
+.Breadcrumb {
+  z-index: 1;
+  position: relative;
+  width: 100%;
+  padding-top: 20vw;
+  font-size: 3.75vw;
+
+  $pc-font-size: 1.6;
+  @include notSp {
+    @include responsiveFontSize($pc-font-size);
+    padding-top: 5rem;
+  }
+
+  @include maxSize {
+    font-size: $pc-font-size + rem;
+  }
+}
+
+.Breadcrumb__inner {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  width: 100%;
+  height: 100%;
+  padding: 0 $horizontalPaddingSp;
+
+  @include notSp {
+    max-width: $maxWidth;
+    padding: 0 $horizontalPadding;
+    margin: auto;
+  }
+}
+
+.List {
+  position: relative;
+  width: 100%;
+  padding: 0.3em;
+  text-align: right;
+
+  @include notSp {
+    text-align: left;
+  }
+}
+
+.List::before {
+  content: '';
+  z-index: 0;
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(to left, rgba($colorWhite, 0.8) 0%, rgba($colorWhite, 0) 80%);
+  user-select: none;
+
+  @include notSp {
+    background: linear-gradient(to right, rgba($colorWhite, 0.8) 0%, rgba($colorWhite, 0) 80%);
+  }
+}
+
+.Cell {
+  position: relative;
+  color: $colorBlack;
+}
+
+@include sp {
+  .Cell + .Cell::after {
+    content: '<';
+    margin-left: 0.5em;
+  }
+}
+
+@include notSp {
+  .Cell + .Cell::before {
+    content: '>';
+    margin-right: 0.5em;
+  }
+}
+
+.Cell:first-child {
+  margin-right: 0.5em;
+
+  @include notSp {
+    margin-left: 0.5em;
+    margin-right: auto;
+  }
+}
+
+.Cell:nth-child(2) {
+  margin-right: 0.5em;
+
+  @include notSp {
+    margin-left: 0.5em;
+    margin-right: auto;
+  }
+}
+
+.Cell:nth-child(3) {
+  margin-right: 1em;
+
+  @include notSp {
+    margin-left: 1em;
+    margin-right: auto;
+  }
+}
+
+.Link {
+  color: currentColor;
+  font-style: italic;
+
+  &,
+  &:hover,
+  &:active {
+    text-decoration: none;
+  }
+}
+</style>
--- src/pages/about/index.vue
+++ src/pages/about/index.vue
@@ -1,41 +1,44 @@
 <!-- eslint-disable -->
 <template>
-  <main id="main" class="Main" role="main">
-    <content-section
-      class="About"
-      title="about kaave.github.io"
-      :desc="[
-        `安部亨佑 a.k.a. kaaveによるWebサイト。
+  <div>
+    <breadcrumb />
+    <main id="main" class="Main" role="main">
+      <content-section
+        class="About"
+        title="about kaave.github.io"
+        :desc="[
+          `安部亨佑 a.k.a. kaaveによるWebサイト。
     主にWeb技術ネタをつらつらと思いつくままに。
     というより、当サイト自体が僕の実験場であります。`,
-      ]"
-    />
-    <section-break />
-    <content-section
-      class="HowToCreate"
-      title="how to create"
-      :desc="[
-        `当サイトはVuePressで構築されており、
+        ]"
+      />
+      <section-break />
+      <content-section
+        class="HowToCreate"
+        title="how to create"
+        :desc="[
+          `当サイトはVuePressで構築されており、
       コードをGitHubにPushするとCircleCIがビルドかけて生成物を配信用リポジトリにPushし、
       配信はGitHub PagesからというVuePress以外は一昔前の構成で制作・運営しております。`,
-      ]"
-    />
-    <section-break />
-    <content-section
-      class="Profile"
-      title="profile"
-      :desc="[
-        `アラフォーのソフトウェアエンジニアです。愛知県在住。静岡県出身。男性。異性愛者。身長181cm、体重68kg。`,
-        `06年中小開発屋へ就職し業界入り。30前後までボサッと働いた後、Web系の案件に割り当てられ真面目化。
+        ]"
+      />
+      <section-break />
+      <content-section
+        class="Profile"
+        title="profile"
+        :desc="[
+          `アラフォーのソフトウェアエンジニアです。愛知県在住。静岡県出身。男性。異性愛者。身長181cm、体重68kg。`,
+          `06年中小開発屋へ就職し業界入り。30前後までボサッと働いた後、Web系の案件に割り当てられ真面目化。
       下積みが長かったのでWeb系ソフトウェアエンジニアがやれることは一通りまぁなんとなくできますが、
       フロントエンドのことを考えているときが一番盛り上がります。
       この界隈はなんというかいつまでもインディー感がありますよね。`,
-        `趣味は音楽鑑賞、自転車、健康法全般、筋トレ(自重)など。
+          `趣味は音楽鑑賞、自転車、健康法全般、筋トレ(自重)など。
       サッカー観戦も好きですが入れ込みすぎて脳汁大量分泌からの睡眠障害、体調不良を引き起こす危険なコンテンツなのでできるだけ触れないようにしています。
       贔屓のチームはアーセナルとジュビロ磐田。`,
-      ]"
-    />
-  </main>
+        ]"
+      />
+    </main>
+  </div>
 </template>
 <!-- eslint-enable -->
 
@@ -57,6 +60,7 @@ import { mapGetters, mapMutations, MutationMethod } from 'vuex';
 
 import ContentSection from '~/components/About/ContentSection.vue';
 import SectionBreak from '~/components/SectionBreak.vue';
+import Breadcrumb from '~/components/Common/Breadcrumb.vue';
 
 type Data = {};
 type Methods = {};
@@ -65,7 +69,7 @@ type Props = {};
 
 const defaultData: Data = {};
 
-const components = { ContentSection, SectionBreak };
+const components = { ContentSection, SectionBreak, Breadcrumb };
 
 export default Vue.extend<Data, Methods, Computed, Props>({
   components,
