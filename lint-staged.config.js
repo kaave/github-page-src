98c0c2270c7671a328a50343e0184e1484a2ebff
kaave <junkjunctions@gmail.com>
2019-06-30 18:17:59 +0900

    fix: add prettier to sass

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,7 +2,7 @@ module.exports = {
   subTaskConcurrency: 1,
   linters: {
     '*.{js,ts}': ['eslint --fix', 'git add'],
-    '*.scss': ['stylelint --fix', 'git add'],
+    '*.scss': ['prettier --write', 'stylelint --fix', 'git add'],
     '*.vue': ['prettier --write', 'stylelint --config .stylelintrc.vue.js', 'eslint --fix', 'git add'],
   },
 };
