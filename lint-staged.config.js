6a1f187eb8e0b684bf46b8260cacb64ac7219d40
kaave <junkjunctions@gmail.com>
2019-07-10 16:34:37 +0900

    npm: upgrade

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,8 +1,5 @@
 module.exports = {
-  subTaskConcurrency: 1,
-  linters: {
-    '*.{js,ts}': ['eslint --fix', 'git add'],
-    '*.scss': ['prettier --write', 'stylelint --fix', 'git add'],
-    '*.vue': ['prettier --write', 'stylelint --config .stylelintrc.vue.js', 'eslint --fix', 'git add'],
-  },
+  '*.{js,ts}': ['eslint --fix', 'git add'],
+  '*.scss': ['prettier --write', 'stylelint --fix', 'git add'],
+  '*.vue': ['prettier --write', 'stylelint --config .stylelintrc.vue.js', 'eslint --fix', 'git add'],
 };
